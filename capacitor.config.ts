0b0b4c23d1171dc395e4a33eb006f60a10d1d9a1
Shubham <shubhamkhale301@gmail.com>
2023-07-20 12:07:46 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gmaps-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
